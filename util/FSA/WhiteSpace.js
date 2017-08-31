83b59298f1cb338e22d971d8252833547a3564c8
Joshua Weinstein <jweinstein@splunk.com>
2017-08-31 16:05:38 -0700

    changed to util dir

---
+++ util/FSA/WhiteSpace.js
@@ -0,0 +1,44 @@
+//contains parsers or state machines for white space
+
+
+var WhiteSpaceBefore = function() {
+	this.final = false;
+	this.index = 0;
+	this.spaceChars = {
+		" ":true, "\n":true, "\t":true
+	}
+	WhiteSpaceBefore.prototype.check = function(char) {
+		if(this.final) return this.index;
+		else if(!(char in this.spaceChars)) {
+			this.final = true;
+			return this.index;
+		}
+		else {
+			this.index++;
+			return false;
+		}
+	};
+};
+
+exports.WhiteSpaceBefore = WhiteSpaceBefore;
+
+var WhiteSpaceAfter = function() {
+	this.final = false;
+	this.index = 0;
+	this.spaceChars = {
+		" ":true, "\n":true, "\t":true
+	}
+	WhiteSpaceAfter.prototype.check = function(char) {
+		if(this.final) return this.index;
+		else if(char in this.spaceChars) {
+			this.final = true;
+			return this.index;
+		}
+		else {
+			this.index++;
+			return false;
+		}
+	};
+};
+
+exports.WhiteSpaceAfter = WhiteSpaceAfter;
