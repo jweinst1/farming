a41a30029fc36705aa5ecf870c3debfe1388399e
Joshua Weinstein <jweinstein@splunk.com>
2017-08-31 16:02:16 -0700

    wrote some whitespace parsers

--- src/FSA/WhiteSpace.js
+++ src/FSA/WhiteSpace.js
@@ -1,6 +1,23 @@
 //contains parsers or state machines for white space
 
 
-exports.WhiteSpaceBefore = function() {
-
+var WhiteSpaceBefore = function() {
+	this.final = false;
+	this.index = 0;
+	this.spaceChars = {
+		" ":true, "\n":true, "\t":true
+	}
+	WhiteSpaceBefore.prototype.check = function(char) {
+		if(this.final) return this.index;
+		else if(char in this.spaceChars) {
+			this.final = true;
+			return this.index;
+		}
+		else {
+			this.index++;
+			return false;
+		}
+	};
 };
+
+exports.WhiteSpaceBefore = WhiteSpaceBefore;
