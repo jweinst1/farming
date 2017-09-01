840c5af8168489d04ef2232e10a57746e0bf1f4d
Joshua Weinstein <jweinstein@splunk.com>
2017-09-01 12:11:52 -0700

    parser updates

--- src/macros/constmacro.js
+++ src/macros/constmacro.js
@@ -3,6 +3,25 @@
 var ConstMacro = function(pattern, replacement) {
 	this.pattern = pattern;
 	this.replace = replacement;
+
+	ConstMacro.prototype.match = function(string) {
+		var matchState = false;
+		var index = 0;
+		for (var i = 0; i < string.length; i++) {
+			if(matchState) {
+				if(this.pattern[++index] == string[i]) {
+					if(index == string.length) return true;
+				}
+				else return false;
+			}
+			else if(this.pattern[index] == string[i]) {
+				matchState = true;
+				index++;
+			}
+
+		}
+		return false;
+	};
 };
 
 exports.ConstMacro = ConstMacro;
---
+++ src/parser/FunctionMacroParser.js
@@ -0,0 +1,13 @@
+//parser for standard function macros
+
+
+//top level parser for function macros
+var FunctionMacroParser = (function(){
+	function FunctionMacroParser(){
+
+	};
+
+	return FunctionMacroParser;
+})();
+
+exports.FunctionMacroParser = FunctionMacroParser;
