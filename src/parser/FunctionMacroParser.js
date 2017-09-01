dc42ff6295b8e153f0bd53ad390fe0f033a370d3
Joshua Weinstein <jweinstein@splunk.com>
2017-09-01 13:58:43 -0700

    defining parser states

--- src/parser/FunctionMacroParser.js
+++ src/parser/FunctionMacroParser.js
@@ -2,10 +2,22 @@
 
 
 //container class for parsed out components of functional macro
+// arbitrary paramter is indicated via a right *, e.g @define set(a*) { [a] }
 var FunctionMacroContainer = function() {
+	this.arbitraryParam = false;
+	this.arbitraryParamName = null;
 	this.name = null;
 	this.params = [];
 	this.textComponents = [];
+
+	FunctionMacroContainer.prototype.setArbitrarySetting = function(option) {
+		this.arbitraryParam = option;
+	};
+
+	FunctionMacroContainer.prototype.setArbitraryName = function(name) {
+		this.arbitraryParamName = name;
+	};
+
 	FunctionMacroContainer.prototype.addParam = function(param) {
 		this.params.push(params);
 	};
@@ -21,11 +33,26 @@ var FunctionMacroContainer = function() {
 
 exports.FunctionMacroContainer = FunctionMacroContainer;
 
+//various states for the parser,
+//named like an enum for convenience
+var FunctionMacroState = {
+	BEGIN:0,
+	FSLASH:1,
+	LINE_COMMENT:2,
+	MLINE_COMMENT:3,
+	STRING:4,
+	TEMP_STRING:5,
+	DEF_SYN:6,
+	DEF_WSPACE:7,
+	DEF_NAME:8
+};
+
+exports.FunctionMacroState = FunctionMacroState;
 
 //top level parser for function macros
 var FunctionMacroParser = (function(){
 	function FunctionMacroParser(){
-
+		this.state = 0;
 	};
 
 	return FunctionMacroParser;
