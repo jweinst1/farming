251d7b6eb2aefa6a51c73a028e15d5244c3a6ab4
Joshua Weinstein <jweinstein@splunk.com>
2017-09-01 13:15:55 -0700

    function macro container

--- src/parser/FunctionMacroParser.js
+++ src/parser/FunctionMacroParser.js
@@ -1,6 +1,27 @@
 //parser for standard function macros
 
 
+//container class for parsed out components of functional macro
+var FunctionMacroContainer = function() {
+	this.name = null;
+	this.params = [];
+	this.textComponents = [];
+	FunctionMacroContainer.prototype.addParam = function(param) {
+		this.params.push(params);
+	};
+
+	FunctionMacroContainer.prototype.addTextComponent = function(text) {
+		this.textComponents.push(text);
+	};
+
+	FunctionMacroContainer.prototype.setName = function(name) {
+		this.name = name;
+	};
+};
+
+exports.FunctionMacroContainer = FunctionMacroContainer;
+
+
 //top level parser for function macros
 var FunctionMacroParser = (function(){
 	function FunctionMacroParser(){
