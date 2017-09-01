f35cd80e6966fd5f2f852a6e0a63a13bbf8f6fa5
Joshua Weinstein <jweinstein@splunk.com>
2017-09-01 16:48:24 -0700

    comment parser finished

--- util/commentparser.js
+++ util/commentparser.js
@@ -24,23 +24,57 @@ var JSCommentParser = (function(){
 		this.comments = [];
 		this.state = CommentState.BASE;
 		this.currentComment = [];
-	};
+	}
 
 	JSCommentParser.prototype.parse = function(string) {
 		for (var i = 0; i < string.length; i++) {
 			switch(this.state) {
 				case CommentState.BASE:
+					if(string[i] === '/') this.state = CommentState.FSLASH;
 					break;
 				case CommentState.FSLASH:
+					if(string[i] === '*') this.state = CommentState.MLINE;
+					else if(string[i] === '/') this.state = CommentState.SLINE;
+					else {
+						//resets if not actual comment init
+						this.state = CommentState.BASE;
+					}
 					break;
 				case CommentState.SLINE:
+					if(string[i] === '\n') {
+						this.state = CommentState.BASE;
+						this.comments.push(this.currentComment.join("")); //ends current comment and pushes to bin
+						this.currentComment = []; //resets current comment
+					}
+					else this.currentComment.push(string[i]);
 					break;
 				case CommentState.MLINE:
+					if(string[i] === '*') this.state = CommentState.END_STAR;
+					else this.currentComment.push(string[i]);
 					break;
 				case CommentState.END_STAR:
+					if(string[i] === '/') {
+						this.state = CommentState.BASE;
+						this.comments.push(this.currentComment.join(""));
+						this.currentComment = [];
+					}
+					else this.currentComment.push('*' + string[i]); //if mutli-line comment does not terminate, puts star as part of comment.
 					break;
 			}
 		}
+		//cleanup 
+		if(this.currentComment.length) this.comments.push(this.currentComment.join(""));
+		this.currentComment = [];
+		this.state = CommentState.BASE;
 	};
+
+	JSCommentParser.prototype.getComments = function() {
+		return this.comments;
+	};
+
+	JSCommentParser.prototype.clearComments = function() {
+		this.comments = [];
+	};	
 	return JSCommentParser;
 })();
+
