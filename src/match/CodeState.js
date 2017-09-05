d23798c1bd4facc3f22c9aa044cedade017d3b2a
Joshua Weinstein <jweinstein@splunk.com>
2017-09-05 16:09:56 -0700

    states update

--- src/match/CodeState.js
+++ src/match/CodeState.js
@@ -3,15 +3,44 @@
 // "foo(4 + 4, 6^5)" ==> doesn't expand
 
 var CodeState = (function(){
-
+	//inner enum like object to name states
 	var innerState = {
-
+		BASE:0,
+		D_QUOTE:1,
+		S_QUOTE:2,
+		TEMP:3,
+		FSLASH:4,
+		SLINE_COMMENT:5,
+		MLINE_COMMENT:6,
+		STAR:7
 	};
 
 
 	function CodeState(){
 		this.isCode = true;
+		this.state = innerState.BASE;
 	}
+
+	CodeState.prototype.input = function(char) {
+		switch(this.state) {
+			case innerState.BASE:
+				break;
+			case innerState.D_QUOTE:
+				break;
+			case innerState.S_QUOTE:
+				break;
+			case innerState.TEMP:
+				break;
+			case innerState.FSLASH:
+				break;
+			case innerState.SLINE_COMMENT:
+				break;
+			case innerState.MLINE_COMMENT:
+				break;
+			case innerState.STAR:
+				break;
+		}
+	};
 	return CodeState;
 })();
 
