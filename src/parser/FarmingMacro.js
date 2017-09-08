af16ecdf00e8313614112013682487169515ae03
Joshua Weinstein <jweinstein@splunk.com>
2017-09-08 00:25:59 -0700

    new main container for macro

---
+++ src/parser/FarmingMacro.js
@@ -0,0 +1,11 @@
+//new file for the main Farming Macro object.
+//Instances of this class are parsed from source code.
+
+var FarmingMacro = (function(){
+  function FarmingMacro(){
+
+  }
+  return FarmingMacro;
+})();
+
+exports.FarmingMacro = FarmingMacro;
