7abe1c4042583fa9d0bb48066f7693a4b7b17490
Joshua Weinstein <jweinstein@splunk.com>
2017-09-13 14:20:46 -0700

    New macro changes

--- src/parser/FarmingMacro.js
+++ src/parser/FarmingMacro.js
@@ -4,18 +4,39 @@
 //small object class to synchronize string storage in macro.
 function StringValue(string) {
   this.str = string;
-  this.toString = function() {
+  StringValue.prototype.toString = function() {
     return this.str;
   }
+}
+
+//closures that set a specific index in the macro's elements
+function makeIndexSetter(index, arr) {
+  return function(strTemp) {
+    arr[index].str = strTemp;
+    return true;
+  };
 }
 
 var FarmingMacro = (function() {
   function FarmingMacro() {
     this.elements = [];
+    this.paramIndexes = {};
   }
 
   FarmingMacro.prototype.addStringElem = function(string) {
     this.elements.push(new StringValue(string));
+  };
+
+  FarmingMacro.prototype.toString = function () {
+    var computeString = "";
+    for (var i = 0; i < this.elements.length; i++) {
+      computeString += this.elements[i].toString();
+    }
+    return computeString;
+  };
+
+  FarmingMacro.prototype.addParam = function (param) {
+    if(!(param in this.paramIndexes)) this.paramIndexes[param] = [];
   };
   return FarmingMacro;
 })();
