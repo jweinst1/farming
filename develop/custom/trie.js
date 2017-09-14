ea50b72e38540ea5800d74436a28e8ea00134ce1
Joshua Weinstein <jweinstein@splunk.com>
2017-09-14 16:26:19 -0700

    trie insert

--- develop/custom/trie.js
+++ develop/custom/trie.js
@@ -16,10 +16,35 @@ var Trie = (function(){
     var last = seq.length-1;
     for (var i = 0; i < seq.length; i++) {
       if(isReserved(seq[i])) throw new Error("Key Error %%END is reserved.");
-      else if(i === last) {
-        
+      else if(seq[i] in currentNode) currentNode = currentNode[seq[i]];
+      else {
+        currentNode[seq[i]] = {};
+        currentNode = currentNode[seq[i]];
       }
     }
+    currentNode["%%END"] = true; //signifies an ending branch
+  };
+
+  Trie.prototype.contains = function (seq) {
+    
   };
   return Trie;
 })();
+/*
+var a = new Trie();
+a.insert("f f g d f".split(" "));
+console.log(JSON.stringify(a.trie, null, 2));
+
+{
+  "f": {
+    "f": {
+      "g": {
+        "d": {
+          "f": {
+            "%%END": true
+          }
+        }
+      }
+    }
+  }
+}*/
