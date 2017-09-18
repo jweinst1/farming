8dcc961ad07782d13cac3c9edf5918bfec9a4340
Joshua Weinstein <jweinstein@splunk.com>
2017-09-17 22:47:42 -0700

    parma map

--- src/macros/IndexParamMap.js
+++ src/macros/IndexParamMap.js
@@ -1,8 +1,10 @@
 //Param -> index map data structure
 
 function ParamMap(params) {
+  this.map = {};
   for (var i = 0; i < params.length; i++) {
-    this[params[i]] = [];
-    
+    this.map[params[i]] = param;
+    this.map[param] = i;
   }
+  this.length = params.length;
 }
