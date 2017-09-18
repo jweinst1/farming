b7cff1c850024f3ff8011afe44a98260c74545f5
Joshua Weinstein <jweinstein@splunk.com>
2017-09-17 19:22:10 -0700

    def parser update

--- src/parser/DefParser.js
+++ src/parser/DefParser.js
@@ -2,11 +2,7 @@
 //also implements def parser containers
 
 
-var ParamIndexMap = function(params) {
-  for (var i = 0; i < params.length; i++) {
-    this[params[i]] = [];
-  }
-}
+
 
 var DefMacroContainer = (function() {
   function DefMacroContainer(){
