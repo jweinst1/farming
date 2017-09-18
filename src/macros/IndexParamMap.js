8991b5de753acfb7d9ea1d473ad656d3c812034b
Joshua Weinstein <jweinstein@splunk.com>
2017-09-17 23:01:41 -0700

    two way adding

--- src/macros/IndexParamMap.js
+++ src/macros/IndexParamMap.js
@@ -1,6 +1,6 @@
 //Param -> index map data structure
 
-function ParamMap(params) {
+var ParamMap = function(params) {
   this.map = {};
   for (var i = 0; i < params.length; i++) {
     this.map[params[i]] = param;
@@ -8,3 +8,10 @@ function ParamMap(params) {
   }
   this.length = params.length;
 }
+
+ParamMap.prototype.add = function (param) {
+  this.map[param] = this.length++;
+  this.map[this.length] = param;
+};
+
+exports.ParamMap = ParamMap;
