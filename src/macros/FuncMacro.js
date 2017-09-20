3f1d4b3e76efff979c609c7d593c505dc599b0b6
Joshua Weinstein <jweinstein@splunk.com>
2017-09-20 11:05:07 -0700

    func macro update

--- src/macros/FuncMacro.js
+++ src/macros/FuncMacro.js
@@ -1,9 +1,23 @@
 //functional macro
 //implements object that can be exported to JSON as well.
-// EXAMPLE ::: name(arg1, arg2) {console.log(arg1, arg2)}
+// EXAMPLE ::: @def name(arg1, arg2) {console.log(arg1, arg2)}
 
-var FuncMacro = function() {
-
+//small object that loads paramter calues
+var DefaultDict = function(){
+  for (var i = 0; i < arguments.length; i++) {
+    this[arguments[i]] = false;
+  }
+  DefaultDict.prototype.add = function (param, val) {
+    val = val || false;
+    this[param] = val;
+  };
 };
+
+var FuncMacro = (function(){
+  function FuncMacro(){
+    this.params = {};
+  }
+  return FuncMacro;
+})();
 
 exports.FuncMacro = FuncMacro;
