199822bb36d3fb0352186cd6d716d7224b24dd7e
Joshua Weinstein <jweinstein@splunk.com>
2017-09-21 23:30:36 -0700

    finished base macro simpler version

--- src/macros/base.js
+++ src/macros/base.js
@@ -9,16 +9,64 @@ var MacroBase = (function() {
     this.params = {};
     this.components = components;
     this.paramOrder = params;
+    this.paramCount = params.length;
     for (var i = 0; i < params.length; i++) {
       this.params[params[i]] = false;
     }
   }
 
-  MacroBase.prototype.makeString = function (args, hasArbitraryParam) {
-    hasArbitraryParam = hasArbitraryParam || false;
-    
+  //cleans past arguments to macro
+  MacroBase.prototype.clean = function () {
+    for (var key in this.params) {
+      this.params[key] = false;
+    }
+  };
+
+  MacroBase.prototype.loadArgs = function (args) {
+    if(args.length !== this.paramCount) {
+      throw new Error("Improper number of arguments for macro.");
+    }
+    for (var i = 0; i < args.length; i++) {
+      this.params[this.paramOrder[i]] = args[i]
+    }
+  };
+
+  MacroBase.prototype.makeString = function (args) {
+    this.loadArgs(args);
+    var makeString = "";
+    var hasArbitraryParam = args[args.length-1].constructor.name === 'Array';
+    if(hasArbitraryParam) {
+      var arbitraryLength = args[args.length-1].length;
+      var arbParam = this.paramOrder[this.paramOrder.length-1];
+      while(arbitraryLength--) {
+        var subArbString = "";
+        for (var i = 0; i < this.components.length; i++) {
+          if(this.components[i] in this.params) {
+            if(this.components[i] === arbParam) {
+              subArbString += this.params[this.components[i]].unshift();
+            }
+            else subArbString += this.params[this.components[i]]
+          }
+          else subArbString += this.components[i];
+        }
+        makeString += subArbString;
+      }
+    }
+    else {
+      for (var i = 0; i < this.components.length; i++) {
+        if(this.components[i] in this.params) {
+          makeString += this.params[this.components[i]];
+        }
+        else makeString += this.components[i];
+      }
+    }
+    this.clean();
+    return makeString;
   };
   return MacroBase;
 })();
 
 exports.MacroBase = MacroBase
+
+var test = new MacroBase(["a"], ["console.log(", "a", ");"]);
+console.log(test.makeString("4"));
