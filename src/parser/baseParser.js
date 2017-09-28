c94e2edbd9efee80d4f103a84d834536d999bf2e
Joshua Weinstein <jweinstein@splunk.com>
2017-09-28 10:04:45 -0700

    base parser improvements

--- src/parser/baseParser.js
+++ src/parser/baseParser.js
@@ -7,13 +7,30 @@ var tkMatch = require('../match/tokenmatch.js');
 //represents states of parser
 var baseParseState = {
   START:0,
-  ATSYM:1,
-
+  //non-parsing states
+  SQUOTE:1,
+  DQUOTE:2,
+  TQUOTE:3,
+  FSLASH:4,
+  SCOMMENT:5,
+  MCOMMENT:6,
+  COMMSTAR:7,
+  //parsing states
+  SYM_AT:8,
+  SYM_D:9,
+  SYM_E:10,
+  SYM_F:11,
+  WS_PRENAME:12,
+  MACNAME:13,
+  PARAM_START:14,
+  PARAM:15,
+  PARAM_COMMA:16,
+  PARAM_END:17
 };
 
 var BaseParser = (function(){
   function BaseParser(){
-    
+
   }
 
   return BaseParser;
